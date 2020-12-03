880164df6e65bd1d797451948975040a843114ae
Neo <teseus@gmail.com>
2020-12-03 23:14:34 +0900

    added a function getting account by token.

--- api/src/main/java/com/base/repository/AccountRepository.java
+++ api/src/main/java/com/base/repository/AccountRepository.java
@@ -2,9 +2,13 @@ package com.base.repository;
 
 import com.base.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findByAccountId(long accountId);
+    @Query("select a from Account a join fetch a.sharing") //Join Fetch N+1 회피.
+    List<Account> findAccountBySharingToken(String token);
 }
