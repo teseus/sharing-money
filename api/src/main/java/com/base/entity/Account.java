7eae4433f29b8941d0a82b0f86207e8f8250d3a4
Neo <teseus@gmail.com>
2020-12-03 23:14:34 +0900

    added a function getting account by token.

--- api/src/main/java/com/base/entity/Account.java
+++ api/src/main/java/com/base/entity/Account.java
@@ -1,6 +1,7 @@
 package com.base.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
@@ -9,12 +10,16 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 @Entity
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Account extends CommonEntity {
     @Id
     private long accountId;
     private long amount;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private Sharing sharing;
 
     @ManyToOne(optional = true, fetch = FetchType.LAZY)
     private User user;
--- api/src/test/groovy/com/base/repository/AccountRepositoryTest.groovy
+++ api/src/test/groovy/com/base/repository/AccountRepositoryTest.groovy
@@ -1,10 +1,12 @@
 package com.base.repository
 
 import com.base.entity.Account
+import com.base.entity.Sharing
 import com.base.entity.User
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.transaction.annotation.Transactional
+import spock.lang.Shared
 import spock.lang.Specification
 
 @SpringBootTest
@@ -13,26 +15,53 @@ class AccountRepositoryTest extends Specification {
     AccountRepository accountRepository
     @Autowired
     UserRepository userRepository
+    @Autowired
+    SharingRepository sharingRepository
+    @Shared
+    def sharing
 
-    def "유저 없이 Account를 저장하고 찾을 수 있어야 한다."() {
+    def setup() {
+        sharing = sharingRepository.save(new Sharing("ABC", "SuiteRoom", 10000L))
+    }
+
+    @Transactional
+    def "Token 으로 Account 들을 찾을 수 있어야한다."() {
         given:
-        def account = new Account(1, 10000, null)
+        def targets = [new Account(1, 3333, sharing, null),
+                       new Account(2, 3333, sharing, null),
+                       new Account(3, 3334, sharing, null)]
+
+        accountRepository.saveAll(targets)
+        when:
+        def accounts = accountRepository.findAccountBySharingToken("ABC")
+        then:
+        accounts.size() == 3
+    }
+
+    @Transactional
+    def "유저 없이 Account 를 저장하고 찾을 수 있어야 한다."() {
+        given:
+        def account = new Account(1, 10000, sharing, null)
         when:
         accountRepository.save(account)
         def result = accountRepository.findByAccountId(1L)
         then:
         result.isPresent()
         result.get().with {
-            amount == 10000
+            getAmount() == 10000
             user == null
+            sharing.with{
+                token == "ABC"
+                roomId == "SuiteRoom"
+            }
         }
     }
 
     @Transactional
-    def "유저 포함 Account를 저장하고 찾을 수 있어야 한다."(){
+    def "유저 포함 Account 를 저장하고 찾을 수 있어야 한다."(){
         given:
         def user = userRepository.save(new User(1L))
-        def account = new Account(1, 10000, user)
+        def account = new Account(1, 10000, sharing, user)
         when:
         accountRepository.save(account)
         def result = accountRepository.findByAccountId(1L)
@@ -41,6 +70,10 @@ class AccountRepositoryTest extends Specification {
         result.get().with {
             amount == 10000
             user != null
+            sharing.with{
+                token == "ABC"
+                roomId == "SuiteRoom"
+            }
         }
     }
 }
