ec618df585e7de1c56308bdd9f63f29d972cea2c
Neo <teseus@gmail.com>
2020-12-06 02:52:39 +0900

    랜덤으로 토큰 3자리 생성하는 것은 TokenEncoder 로 대체되어 지움.

--- api/src/main/java/com/base/service/SharingApplicationServiceHelper.java
+++ api/src/main/java/com/base/service/SharingApplicationServiceHelper.java
@@ -2,41 +2,8 @@ package com.base.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class SharingApplicationServiceHelper {
-    private static final int TOKEN_SIZE = 3;
-    private static final int ASCII_ZERO = 48;
-    private static final int ASCII_SMALL_Z = 122;
-
-    public static String generate3LengthToken() {
-        return new Random().ints(ASCII_ZERO, ASCII_SMALL_Z + 1)
-                .filter(SharingApplicationServiceHelper::isAlphabetOrNumber)
-                .limit(TOKEN_SIZE)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
-    }
-
-    private static boolean isAlphabetOrNumber(int i) {
-        return (isUnderMaxNumeric(i) || isOverA(i)) && (isUnderZ(i) || isOverLowerA(i));
-    }
-
-    private static boolean isOverLowerA(int i) {
-        return i >= 97;
-    }
-
-    private static boolean isUnderZ(int i) {
-        return i <= 90;
-    }
-
-    private static boolean isOverA(int i) {
-        return i >= 65;
-    }
-
-    private static boolean isUnderMaxNumeric(int i) {
-        return i <= 57;
-    }
-
     public static List<Long> separateMoney(long totalMoney, long separateNumber) {
         long unit = totalMoney / separateNumber;
         List<Long> separatedValues = new ArrayList<>();
--- api/src/test/groovy/com/base/service/SharingApplicationServiceHelperTest.groovy
+++ api/src/test/groovy/com/base/service/SharingApplicationServiceHelperTest.groovy
@@ -5,14 +5,6 @@ import spock.lang.Specification
 class SharingApplicationServiceHelperTest extends Specification {
     public static final int TOTAL_AMOUNT = 10000000
 
-    def "생성한 토큰의 자리수는 3이 되어야 한다."(){
-        when:
-        def token = SharingApplicationServiceHelper.generate3LengthToken()
-        then:
-        token.size() == 3
-        println(token)
-    }
-
     def "액수를 인원수로 나눈 것들의 합이 원래의 금액이 되어야 한다."() {
         when:
         List<Long> separated = SharingApplicationServiceHelper.separateMoney(TOTAL_AMOUNT, 3)
