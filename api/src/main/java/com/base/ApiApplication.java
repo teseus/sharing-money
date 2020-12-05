eea519aa2f9b714dda5eddabb0c18a8c0fec4adc
Neo <teseus@gmail.com>
2020-12-06 02:43:30 +0900

    Application 에 @EnableJpaAudinting 이동으로 JpaAuditing.java 제거했음.

--- api/src/main/java/com/base/ApiApplication.java
+++ api/src/main/java/com/base/ApiApplication.java
@@ -2,7 +2,9 @@ package com.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication(scanBasePackages = {"com.base"})
 public class ApiApplication {
     public static void main(String[] args) {
