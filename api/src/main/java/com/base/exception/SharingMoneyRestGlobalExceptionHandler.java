65bff31fafe902bcd49c2cc307e73e79e8aa68b2
Neo <teseus@gmail.com>
2020-12-06 21:08:51 +0900

    refactored to wrap code up.

--- api/src/main/java/com/base/exception/SharingMoneyRestGlobalExceptionHandler.java
+++ api/src/main/java/com/base/exception/SharingMoneyRestGlobalExceptionHandler.java
@@ -13,31 +13,36 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 public class SharingMoneyRestGlobalExceptionHandler {
-    @ExceptionHandler(value = {ConstraintViolationException.class, IllegalArgumentException.class, IllegalStateException.class})
+    @ExceptionHandler(value = {ConstraintViolationException.class,
+            IllegalArgumentException.class, IllegalStateException.class})
     public ResponseEntity<ErrorMessageDTO> catchIllegalArgument(RuntimeException ex) {
-        log.error("{}", ex.getMessage(), ex);
-        ErrorMessageDTO errorMessageDTO = new ErrorMessageDTO();
-        errorMessageDTO.addMessage("error", ex.getClass().getName());
-        errorMessageDTO.addMessage("reason", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessageDTO);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(assbleErrorMessageDTO(ex, ex.getMessage()));
     }
 
     @ExceptionHandler(value = {DataIntegrityViolationException.class})
     public ResponseEntity<ErrorMessageDTO> catchDataViolation(DataIntegrityViolationException ex) {
-        log.error("{}", ex.getMessage(), ex);
-        ErrorMessageDTO errorMessageDTO = new ErrorMessageDTO();
-        errorMessageDTO.addMessage("error", ex.getClass().getName());
-        errorMessageDTO.addMessage("reason", "같은 토큰으로는 두번이상 받을 수 없습니다.");
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorMessageDTO);
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(assbleErrorMessageDTO(ex, "같은 토큰으로는 두번이상 받을 수 없습니다."));
     }
-
 
     @ExceptionHandler(value = {MethodArgumentNotValidException.class})
     public ResponseEntity<ErrorMessageDTO> catchBadRequest(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(assbleErrorMessageDTO(ex, ex.getMessage()));
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<ErrorMessageDTO> catchOthers(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(assbleErrorMessageDTO(ex, ex.getMessage()));
+    }
+
+    private ErrorMessageDTO assbleErrorMessageDTO(Exception ex, String message) {
         log.error("{}", ex.getMessage(), ex);
         ErrorMessageDTO errorMessageDTO = new ErrorMessageDTO();
         errorMessageDTO.addMessage("error", ex.getClass().getName());
-        errorMessageDTO.addMessage("reason", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessageDTO);
+        errorMessageDTO.addMessage("reason", message);
+        return errorMessageDTO;
     }
 }
